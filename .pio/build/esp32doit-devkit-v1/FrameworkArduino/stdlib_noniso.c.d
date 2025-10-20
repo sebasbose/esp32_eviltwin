064f6395b1c05d8e5fea7e66a82592ad5610ee30
Sebastián Bolaños <sebasbose@gmail.com>
2025-10-19 21:16:01 -0500

    Initial implementation of PlatformIO project files.

---
+++ .pio/build/esp32doit-devkit-v1/FrameworkArduino/stdlib_noniso.c.d
@@ -0,0 +1,12 @@
+.pio/build/esp32doit-devkit-v1/FrameworkArduino/stdlib_noniso.c.o: \
+ /Users/sebasbose/.platformio/packages/framework-arduinoespressif32/cores/esp32/stdlib_noniso.c \
+ /Users/sebasbose/.platformio/packages/framework-arduinoespressif32/cores/esp32/stdlib_noniso.h \
+ /Users/sebasbose/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_system/include/esp_system.h \
+ /Users/sebasbose/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_common/include/esp_err.h \
+ /Users/sebasbose/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_common/include/esp_compiler.h \
+ /Users/sebasbose/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_common/include/esp_attr.h \
+ /Users/sebasbose/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_common/include/esp_bit_defs.h \
+ /Users/sebasbose/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_common/include/esp_idf_version.h \
+ /Users/sebasbose/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_hw_support/include/esp_mac.h \
+ /Users/sebasbose/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_hw_support/include/esp_chip_info.h \
+ /Users/sebasbose/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_hw_support/include/esp_random.h
