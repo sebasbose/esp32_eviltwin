9cc9f59167b8440dc5154049fe6fe67b84c2dc96
Sebastián Bolaños <sebasbose@gmail.com>
2025-10-19 21:17:21 -0500

    Cleaned repo

--- .pio/build/esp32doit-devkit-v1/FrameworkArduino/FirmwareMSC.cpp.d
+++
@@ -1,5 +0,0 @@
-.pio/build/esp32doit-devkit-v1/FrameworkArduino/FirmwareMSC.cpp.o: \
- /Users/sebasbose/.platformio/packages/framework-arduinoespressif32/cores/esp32/FirmwareMSC.cpp \
- /Users/sebasbose/.platformio/packages/framework-arduinoespressif32/cores/esp32/FirmwareMSC.h \
- /Users/sebasbose/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBMSC.h \
- /Users/sebasbose/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/dio_qspi/include/sdkconfig.h
--- .pio/build/esp32doit-devkit-v1/FrameworkArduino/cbuf.cpp.d
+++
@@ -1,3 +0,0 @@
-.pio/build/esp32doit-devkit-v1/FrameworkArduino/cbuf.cpp.o: \
- /Users/sebasbose/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.cpp \
- /Users/sebasbose/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.h
